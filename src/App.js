6e27afb36166c1ffde18a71424eed28c1b8b91d3
unknown <andradedelira1906@gmail.com>
2023-05-23 17:15:11 -0300

    Alterar map, produto e etc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from "react"
 import { Text, View, StyleSheet } from 'react-native'
-import Familia from "./componentes/relacao/Familia"
-import Membro from "./componentes/relacao/Membro"
+import ListaProdutos from "./componentes/produto/ListaProdutos"
+import ListaProdutoV2 from "./componentes/produto/ListaProdutoV2"
 import { SafeAreaView } from "react-native-safe-area-context"
 
+// const Stack = createNativeStackNavigator()
+
 export default () => (
-    <SafeAreaView style={styles.TelaIniciante}>
-    <Familia>
-        <Membro nome="Jorge" sobrenome="Pereiras"/>
-        <Membro nome="Ednaldo" sobrenome="Pereiras"/>
-        <Membro nome="Marcinho" sobrenome="Pereiras"/>
-        <Membro nome="Roberta" sobrenome="Pereiras"/>
-    </Familia>
-    <Familia>
-        <Membro nome="Lucas" sobrenome="da Silva"/>
-        <Membro nome="Fernando" sobrenome="da Silva"/>
-        <Membro nome="Jorge" sobrenome="da Silva"/>
-        <Membro nome="Marcia" sobrenome="da Silva"/>
-        </Familia>
+   <SafeAreaView style={styles.TelaIniciante}>
+    <ListaProdutoV2/>
    </SafeAreaView>
 )
+
 const styles = StyleSheet.create({
     TelaIniciante: {
         flexGrow: 1,
